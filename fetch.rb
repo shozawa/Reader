762e8d74b3ceaaa09e94d0e039dc13fa5c067310
shozawa tomohiro <t.shozawa@gmail.com>
2015-09-15 19:11:39 +0900

    RSSのパース関数を追加

--- fetch.rb
+++ fetch.rb
@@ -1,1 +1,35 @@
+require 'sqlite3'
+require 'rss'
+require 'open-uri'
+require 'time'
+require 'uri'
 
+def parse_url(source)
+  rss = RSS::Parser.parse(source)
+  articles =  rss.items
+  articles.each do |article|
+    title = article.title
+    link = article.link
+    date = article.date.to_i
+    p "#{date} : #{Time.at(date)}"
+  end
+end
+
+def subscribe(source)
+  if url?(source)
+    File.open('source.txt', 'a') do |f|
+      f.puts source
+    end
+  else
+  end
+end
+
+def url?(source)
+  begin
+    scheme = URI(source).scheme
+    p scheme
+  rescue
+    ''
+  end
+  %w(http https).include?(scheme)
+end
---
+++ parse_test.rb
@@ -0,0 +1,10 @@
+require './fetch'
+
+url = 'http://blog.codebook-10000.com/rss'
+
+
+subscribe(url)
+
+
+
+
