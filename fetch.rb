cddcba1973b0e8e5e45025b2700e152f2ad07f45
shozawa tomohiro <t.shozawa@gmail.com>
2015-09-16 09:11:41 +0900

    RSSのバリデーションを追加

--- fetch.rb
+++ fetch.rb
@@ -2,34 +2,48 @@ require 'sqlite3'
 require 'rss'
 require 'open-uri'
 require 'time'
-require 'uri'
+
+def update
+  feeds.each do |feed|
+    parse_url(feed)
+  end
+end
+
+def feeds
+  File.open('source.txt') do |f|
+    f.read.split("\n")
+  end
+end
 
 def parse_url(source)
   rss = RSS::Parser.parse(source)
+  media = rss.channel.link
   articles =  rss.items
   articles.each do |article|
     title = article.title
     link = article.link
     date = article.date.to_i
-    p "#{date} : #{Time.at(date)}"
+    puts "- #{media} |  #{title} : #{date}"
   end
 end
 
 def subscribe(source)
-  if url?(source)
-    File.open('source.txt', 'a') do |f|
-      f.puts source
-    end
-  else
+  return unless rss?(source)
+  return unless new_feed?(source)
+  File.open('source.txt', 'a') do |f|
+    f.puts source
   end
 end
 
-def url?(source)
-  begin
-    scheme = URI(source).scheme
-    p scheme
-  rescue
-    ''
+def new_feed?(source)
+  File.open('source.txt') do |f|
+    urls = f.read.split("\n")
+    !urls.include?(source)
   end
-  %w(http https).include?(scheme)
+end
+
+def rss?(source)
+  true if RSS::Parser.parse(source)
+rescue
+  false
 end
--- parse_test.rb
+++ parse_test.rb
@@ -1,9 +1,6 @@
 require './fetch'
 
-url = 'http://blog.codebook-10000.com/rss'
-
-
-subscribe(url)
+update
 
 
 
